e7991bdc3eb8186fd0240ed3fa836656900eceab
Haowen Hou <haowenhou@outlook.com>
2024-04-17 20:34:45 -0700

    Fix icon svg bug

--- pages/components/Sidebar/index.js
+++ pages/components/Sidebar/index.js
@@ -4,9 +4,10 @@ const Sidebar = () => {
     return (
         <aside className="bg-gray-50 w-64 h-screen">
             <div className='p-4 flex'>
-                <svg className='size-10 stroke-orange-500' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 stroke-orange-500">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z" />
                 </svg>
+
                 <span className='text-3xl pl-4 pt-0.5 font-bold'>Zero Music</span>
             </div>
             <nav className='text-left pt-8 ml-4 mr-2 flex flex-col gap-2'>
