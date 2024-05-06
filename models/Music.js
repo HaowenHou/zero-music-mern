f9f28b67b71d900a1fb21f79d034a3478c58f36f
Haowen Hou <haowenhou@outlook.com>
2024-05-06 19:00:48 +0800

    Save track to database

--- models/Music.js
+++ models/Music.js
@@ -4,7 +4,8 @@ const musicSchema = new mongoose.Schema({
   name: { type: String, required: true },
   artist: { type: String, required: true },
   duration: { type: Number, required: true }, // duration in seconds
-  cover: { type: String, required: true } // URL to the cover image
+  cover: { type: String, required: true }, // URL to the cover image
+  track: { type: String, required: true } // URL to the music file
 });
 
 export default mongoose.models.Music || mongoose.model('Music', musicSchema);
--- pages/api/uploadMusic.js
+++ pages/api/uploadMusic.js
@@ -1,14 +1,19 @@
-import { log } from "console";
 import formidable from "formidable";
 import fs from "fs";
 import path from "path";
 
+import dbConnect from '@/lib/dbConnect';
+import Music from '@/models/Music';
+
 export default async function handle(req, res) {
   const { method } = req;
+  var musicDuration = 0;
+  var trackPath = '';
+  var coverPath = '';
 
   if (method == 'POST') {
     const form = formidable({});
-    form.parse(req, (err, fields, files) => {
+    form.parse(req, async (err, fields, files) => {
       if (err) {
         res.status(400).json({ error: err.message });
         res.end();
@@ -17,34 +22,47 @@ export default async function handle(req, res) {
 
       const newName = `${fields.name} - ${fields.artist}`;
 
+      // Save the music file
+      const musicMetadata = require('music-metadata');
       if (files.track) {
         const file = Array.isArray(files.track) ? files.track[0] : files.track;
         const uploadsDir = path.resolve('./public/tracks');
         const oldPath = file.filepath;
         const newPath = path.join(uploadsDir, newName + path.extname(file.originalFilename));
+        trackPath = newPath;
 
         if (!fs.existsSync(uploadsDir)) {
           fs.mkdirSync(uploadsDir, { recursive: true }); // 'recursive: true' ensures that nested directories are created
         }
 
         // Move the file to the new location
-        fs.rename(oldPath, newPath, (err) => {
+        fs.rename(oldPath, newPath, async (err) => {
           if (err) {
             console.error('Error moving the file', err);
             res.status(500).json({ error: 'Failed to move file' });
             return;
           }
-          res.json({ message: 'File uploaded successfully', path: newPath });
+
+          // Get the duration
+          try {
+            const metadata = await musicMetadata.parseFile(newPath);
+            musicDuration = Math.round(metadata.format.duration);  // Duration in seconds
+          } catch (metadataError) {
+            console.error('Error reading metadata', metadataError);
+            res.status(500).json({ error: 'Failed to read file metadata' });
+          }
         });
       } else {
         res.status(400).json({ error: 'No file uploaded' });
       }
 
+      // Save the cover image
       if (files.cover) {
         const file = Array.isArray(files.cover) ? files.cover[0] : files.cover;
         const uploadsDir = path.resolve('./public/covers');
         const oldPath = file.filepath;
         const newPath = path.join(uploadsDir, newName + path.extname(file.originalFilename));
+        coverPath = newPath;
 
         if (!fs.existsSync(uploadsDir)) {
           fs.mkdirSync(uploadsDir, { recursive: true }); // 'recursive: true' ensures that nested directories are created
@@ -57,9 +75,21 @@ export default async function handle(req, res) {
             res.status(500).json({ error: 'Failed to move file' });
             return;
           }
-          res.json({ message: 'File uploaded successfully', path: newPath });
+
+
         });
       }
+
+      // Save to the database
+      await dbConnect();
+      const music = new Music({
+        name: fields.name[0],
+        artist: fields.artist[0],
+        duration: musicDuration,
+        cover: coverPath,
+        track: trackPath,
+      });
+      await music.save();
     });
   }
 }
