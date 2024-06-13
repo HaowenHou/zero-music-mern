f9cf7ad70151a3f3af409b8efbeaef6f416a48db
Haowen Hou <haowenhou@outlook.com>
2024-06-13 15:01:38 +0800

    Create Express backend

---
+++ zero-music-backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config()
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
