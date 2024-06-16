ae8396ab3d49da6b2418b92f1cd5ea11287222a3
Haowen Hou <haowenhou@outlook.com>
2024-06-16 19:13:59 +0800

    Frontend: Update search result UI

--- zero-music-frontend/src/pages/search/[q].jsx
+++ zero-music-frontend/src/pages/search/[q].jsx
@@ -14,28 +14,30 @@ const SearchResults = () => {
   if (!results) return null;
 
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-semibold">搜索结果</h1>
-      {!!results.tracks.length && (
-        <>
-          <h2 className="text-lg font-semibold mt-4">音乐</h2>
-          <Tracklist tracks={results.tracks} showFavoriteButton={false} />
-        </>
-      )}
-      {!!results.users.length && (
-        <>
-          <h2 className="text-lg font-semibold mt-4 mb-2">用户</h2>
-          {results.users.map(item =>
-            <Link to={`/profile/${item._id}`} key={item._id} className="p-2 flex items-center">
-              <img
-                src={import.meta.env.VITE_SERVER_URL + (item.avatar || '/assets/default-cover-1.png')}
-                alt="Avatar"
-                className="w-12 h-12 mr-6 rounded-md"
-              />
-              <h2 className="font-semibold ml-6">{item.name}</h2>
-            </Link>)}
-        </>
-      )}
+    <div className="p-12">
+      <h1 className="text-2xl font-bold">搜索结果</h1>
+      <div className='px-8 mt-10'>
+        {!!results.tracks.length && (
+          <>
+            <h2 className="text-lg font-semibold mt-4">音乐</h2>
+            <Tracklist tracks={results.tracks} showFavoriteButton={false} />
+          </>
+        )}
+        {!!results.users.length && (
+          <>
+            <h2 className="text-lg font-semibold mt-8 mb-8">用户</h2>
+            {results.users.map(item =>
+              <Link to={`/profile/${item._id}`} key={item._id} className="p-2 flex items-center">
+                <img
+                  src={import.meta.env.VITE_SERVER_URL + (item.avatar || '/assets/default-cover-1.png')}
+                  alt="Avatar"
+                  className="w-12 h-12 mr-6 rounded-md"
+                />
+                <h2 className="font-semibold ml-28">{item.name}</h2>
+              </Link>)}
+          </>
+        )}
+      </div>
     </div>
   );
 };
