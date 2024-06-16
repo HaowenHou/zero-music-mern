8d7f3fcf90d286a79031b6210fd9281e23c8b8ca
Haowen Hou <haowenhou@outlook.com>
2024-06-16 15:26:39 +0800

    Update post UI

--- zero-music-frontend/src/components/PlayerControl.jsx
+++ zero-music-frontend/src/components/PlayerControl.jsx
@@ -155,15 +155,15 @@ const PlayerControl = () => {
           </div>
 
           <div className='flex flex-[3] items-center justify-center gap-4'>
-            {/* {playlist[currentTrackIndex].favorite ? (
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7 ml-4">
-            <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
-          </svg>
-        ) : (
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 ml-4">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-          </svg>
-        )} */}
+            {playlist[currentTrackIndex].isFavorited ? (
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7 ml-4">
+                <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+              </svg>
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 ml-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+              </svg>
+            )}
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
               <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
             </svg>
--- zero-music-frontend/src/pages/posts/new.jsx
+++ zero-music-frontend/src/pages/posts/new.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 
 export default function New() {
   const [content, setContent] = useState('');
+  const [selectedTrack, setSelectedTrack] = useState(null);
   const [trackId, setTrackId] = useState(null);
   const [tracks, setTracks] = useState([]); // This will hold the search results
   const [searchTerm, setSearchTerm] = useState('');
@@ -13,8 +14,13 @@ export default function New() {
 
   const handleSearch = async (event) => {
     setSearchTerm(event.target.value);
+    if (!event.target.value) {
+      setTracks([]);
+      return;
+    }
     try {
       const response = await axios.get(import.meta.env.VITE_SERVER_URL + `/api/search?q=${searchTerm}`);
+      console.log(response.data.tracks)
       setTracks(response.data.tracks);
     } catch (error) {
       console.error(error);
@@ -35,41 +41,62 @@ export default function New() {
     navigate('/posts');
   };
 
+
+
+  const handleCancel = () => {
+    setTrackId(null);
+    setSelectedTrack(null);
+    setSearchTerm('');
+  };
+
   return (
-    <div className="flex m-4 space-x-2">
+    <div className="flex justify-center m-12 mx-24 space-x-2">
       <div className="w-1/2">
+        <h1 className="text-2xl font-bold">发表动态</h1>
         <form>
           <textarea
-            className="w-full h-32 p-2 rounded-lg"
+            className="w-full h-32 p-2 rounded-lg mt-12"
             placeholder="想说点什么？"
             value={content}
             onChange={(e) => setContent(e.target.value)}
           />
-          <button onClick={handleSubmit} className="bg-orange-400 text-white rounded-lg px-4 py-2 mt-2">
-            发布
-          </button>
         </form>
-      </div>
-
-      <div className="w-1/2">
         <input
           type="text"
-          placeholder="Search for tracks"
+          placeholder="带上音乐"
           value={searchTerm}
           onChange={handleSearch}
-          className="w-full p-2 border rounded"
+          className="w-full p-2 border rounded mt-12"
         />
+        {trackId && (
+          <div className='flex m-2 items-center'>
+            {selectedTrack.title}
+            <img src={import.meta.env.VITE_SERVER_URL + selectedTrack.cover} alt=""
+              className='ml-auto rounded-lg h-8 w-8' />
+            <button onClick={handleCancel} className="border border-red-400 rounded-lg py-1 px-2 ml-1.5 hover:bg-red-400">
+              取消
+            </button>
+          </div>
+        )}
         <ul className="mt-2">
           {!!tracks.length && tracks.map(track => (
             <li
               key={track._id}
-              className={`p-2 ${trackId === track._id ? 'bg-gray-300' : 'hover:bg-gray-100'} cursor-pointer`}
-              onClick={() => setTrackId(track._id)}
+              className={`p-2 ${trackId === track._id ? 'bg-gray-300' : 'hover:bg-gray-100'} cursor-pointer rounded-lg`}
+              onClick={() => { setTrackId(track._id); setSelectedTrack(track); setSearchTerm(track.title); }}
             >
-              {track.title}
+              <div className='flex items-center'>
+                {track.title}
+                <img src={import.meta.env.VITE_SERVER_URL + track.cover} alt=""
+                  className='ml-auto rounded-lg h-8 w-8' />
+              </div>
             </li>
           ))}
         </ul>
+
+        <button onClick={handleSubmit} className="bg-orange-400 text-white rounded-lg px-4 py-2 mt-2">
+          发布
+        </button>
       </div>
     </div>
   );
